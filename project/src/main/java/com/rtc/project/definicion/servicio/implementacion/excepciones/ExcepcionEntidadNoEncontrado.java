c1fa58aaac935c7e00693e6c3fb2a83e32561642
Dyson <dysontilano@gmail.com>
2022-10-11 11:46:07 -0500

    Updated license of '*.java' files

--- project/src/main/java/com/rtc/project/definicion/servicio/implementacion/excepciones/ExcepcionEntidadNoEncontrado.java
+++ project/src/main/java/com/rtc/project/definicion/servicio/implementacion/excepciones/ExcepcionEntidadNoEncontrado.java
@@ -1,13 +1,13 @@
 /*
- * @fileoverview {FileName} se encarga de realizar tareas especificas.
+ * @fileoverview {ExcepcionEntidadNoEncontrado} se encarga de realizar tareas especificas.
  *
  * @version             1.0
  *
  * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
  * Copyright (C) Dyson Parra
  *
- * @History v1.0 --- La implementacion de {FileName} fue realizada el 31/07/2022.
- * @Dev - La primera version de {FileName} fue escrita por Dyson A. Parra T.
+ * @History v1.0 --- La implementacion de {ExcepcionEntidadNoEncontrado} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {ExcepcionEntidadNoEncontrado} fue escrita por Dyson A. Parra T.
  */
 package com.rtc.project.definicion.servicio.implementacion.excepciones;
 
